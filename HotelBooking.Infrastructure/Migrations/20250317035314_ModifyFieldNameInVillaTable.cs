a7aa819513ec2b522f9a220ac2557ad04120bcaf
Abul-Kashem <kashem.mcpd@gmail.com>
2025-03-17 10:18:45 +0600

    VillaTableCreateWithSeedData

---
+++ HotelBooking.Infrastructure/Migrations/20250317035314_ModifyFieldNameInVillaTable.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelBooking.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifyFieldNameInVillaTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UpdateDate",
+                table: "Villas",
+                newName: "Updated_Date");
+
+            migrationBuilder.RenameColumn(
+                name: "CreateDate",
+                table: "Villas",
+                newName: "Created_Date");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Updated_Date",
+                table: "Villas",
+                newName: "UpdateDate");
+
+            migrationBuilder.RenameColumn(
+                name: "Created_Date",
+                table: "Villas",
+                newName: "CreateDate");
+        }
+    }
+}
