83aa7817d20093ae32d7d81490950fdae3e17370
Abul-Kashem <kashem.mcpd@gmail.com>
2025-04-02 22:12:27 +0600

    Add project files.

---
+++ HotelBooking.Infrastructure/Migrations/20250317040720_ModifyFieldNameOccupancyInVillaTable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelBooking.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifyFieldNameOccupancyInVillaTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Occupency",
+                table: "Villas",
+                newName: "Occupancy");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Occupancy",
+                table: "Villas",
+                newName: "Occupency");
+        }
+    }
+}
